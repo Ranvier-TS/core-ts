ffc102b59e4aac622efad087761e36c6a1d43992
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2020-10-22 00:52:04 -0400

    Debug log, update node types

--- src/RoomManager.ts
+++ src/RoomManager.ts
@@ -17,6 +17,7 @@ export class RoomManager {
 	getRoom(entityRef: EntityReference) {
 		const room = this.rooms.get(entityRef);
 		if (!room) {
+			console.log(this.rooms);
 			throw new Error(`RoomManager can't find the Room [${entityRef}]`);
 		}
 		return room;
